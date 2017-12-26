e314df298198987b493dfa578420c9f9990ba07c
Erol Keskiner <erolksknr@gmail.com>
2017-12-27 00:05:07 +0300

    simple version for more functionality

--- chunkManagement/separateAndUnite.py
+++ chunkManagement/separateAndUnite.py
@@ -1,73 +1,78 @@
 import hashlib
 import os
 import time
+import pickle
 
-writepath = 'outTest/'
+
 
 def md5sum(filename, blocksize=2097152):
+
     hash = hashlib.md5()
-    with open(filename, "rb") as f:
+    with open('shared/'+filename, "rb") as f:
         for block in iter(lambda: f.read(blocksize), b""):
             hash.update(block)
     return hash.hexdigest()
 
-def separete_to_chunks(filename,blocksize,mSum,N):
+def separete_to_chunks(filename,blocksize):
+    filePath = 'shared/'+filename
+    mSum = md5sum(filename)
+    with open(filePath, "rb") as f:
+        content = f.read()
 
-    with open(filename, "rb") as f:
-        with open(str(mSum)+'.meta','w+') as meta:
-            meta.write('FileName:' + f.name + '\n')
-            statinfo = os.stat(f.name)
-            meta.write('FileSize:'+str(statinfo.st_size)+'\n')
-            meta.write('MD5SUM:'+mSum+'\n')
-            meta.write('NumberOfChunks:'+str(N)+'\n')
+        fn, file_extension = os.path.splitext(filename)
 
-        for i in range(N):
+        chunkNumber = int(len(content) / blocksize) + 1
+        statinfo = os.stat(f.name)
+        dict = {'FileName': filename, 'FileSize':str(statinfo.st_size),'MD5SUM':mSum,'NumberOfChunks':chunkNumber}
+
+        pickle_out = open('meta/'+str(mSum)+'.pickle', 'wb')
+        pickle.dump(dict,pickle_out)
+        pickle_out.close()
+
+
+    with open(filePath,"rb") as f:
+        for i in range(chunkNumber):
             with open('tmp/'+mSum+'-{}'.format(i),'wb') as o:
                 o.write(f.read(blocksize))
 
-def chunks_to_file(mSum,c):
+def chunks_to_file(mSum):
+    pickle_in = open('meta/'+str(mSum)+'.pickle', 'rb')
+    dict = pickle.load(pickle_in)
+    pickle_in.close()
+    fileName = dict['FileName']
+    print(dict['FileName'])
+    fn, ext = os.path.splitext(fileName)
+    c = dict['NumberOfChunks']
 
-    with open('out1.pdf','wb') as o:
+
+    with open('downloaded/'+fn+ext,'wb') as o:
         for i in range(c):
             with open('tmp/'+mSum+'-{}'.format(i),'rb') as chunk:
                 o.write(chunk.read())
+        for i in range(c):
+            os.remove('tmp/'+mSum+'-{}'.format(i))
 
 
 
-p = time.time()
 
-fileName ='asd.pdf'
 
-with open(fileName,'rb') as f:
-    content = f.read()
-    print (len(content))
-
-    chunkSize = 10240 # as bytes
-
-    fn, file_extension = os.path.splitext(fileName)
-
-    print('File name: ')
-    print(fn)
-    print('File extension: ')
-    print(file_extension)
-
-    print (len(content)/chunkSize)
-    chunkNumber = int(len(content)/chunkSize) + 1
-    lastChunkSize = len(content) % chunkSize
-    print('chunk sayısı')
-    print(chunkNumber)
-    print(lastChunkSize)
-    m = md5sum(f.name)
-    print('md5')
-    print (type(m))
-    #separete_to_chunks(fileName,2097152,m,chunkNumber)
-    chunks_to_file(m,chunkNumber)
+fileName ='asd.jpg'
+chunkSize = 10240 # as bytes
 
 
 
-pEnd = time.time()
 
-print (str(pEnd - p))
+m = md5sum(fileName)
+print(m)
+
+
+#separete_to_chunks(fileName,chunkSize)
+chunks_to_file(m)
+
+
+
+
+
 
     
     
